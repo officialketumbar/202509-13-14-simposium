1c9f9edece53000ecf758a879cffe73d82c53caa
officialketumbar <officialketumbar@gmail.com>
2025-09-05 10:46:36 +0700

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // 1. CONFIG (tanpa spasi di akhir!)
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbybKVEgjhovLsYkeb0sTdCZBaYJnOrCTenGxAv8fiAQc0jWS03U27yb9jMg5pvYToA9/exec';
+const SCRIPT_URL ='https://script.google.com/macros/s/AKfycbwFVM58W9nB3pjLMH-ddjBsHeQShzLYqRog-2VGhLlW3CI96mLgeSGUenoG52KEFYdb/exec';
 // 2. UI HELPER -------------------------------------------------
 function showLoading() {
   document.getElementById('loadingOverlay').style.display = 'flex';
@@ -123,6 +123,7 @@ document.getElementById('nikInput').addEventListener('input', (e) => {
     // checkNIK();
   }
 });
+
 
 
 
