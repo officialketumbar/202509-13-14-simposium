2f1dbfe01d0065579137a2358c97e555a146ee37
officialketumbar <officialketumbar@gmail.com>
2025-09-05 13:21:14 +0700

    config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,5 @@
 // GANTI DENGAN URL ANDA (tanpa spasi / trailing slash)
 const CONFIG = {
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbyW6H3q_1SHeIqNL16BIRGhuBm1S4tg64Wy7S1yqKP4iwEL9Ccds5L9C7WIFrS2pZ14/exec'
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbzjQccp8YpDWoMKCuEyQwq2RRiJgUa9lUkG5w2sB6JXTwUB1AHsvaINDwnpy4u0Aunh/exec'
 };
+
